3ef5cd33b142cfd2f55cf92789246f91bea0de11
Natka <natalia.kur@yahoo.com>
2021-02-07 00:44:56 +0100

    Started creating the forecast for upcoming hours
    Removed the dummy layout for the upcoming weather forecast and started creating the forecast for the upcoming hours.

--- src/index.js
+++ src/index.js
@@ -58,11 +58,33 @@ function showTemperature(response) {
   ).innerHTML = `${pressure}${pressureUnit}`;
   celsiusTemp = response.data.main.temp; // can I use the variable temperature instead of response.data.main.temp?
 }
+
+function showForecast(response) {
+  console.log(response.data);
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.list[0];
+  console.log(forecast);
+  forecastElement.innerHTML = `
+    <div class="col-2">
+      <h4>12:00</h4>
+      <img src="https://openweathermap.org/img/wn/${
+        forecast.weather[0].icon
+      }@2x.png"/>
+      <div class="forecast-temperature">
+      <strong>${Math.round(forecast.main.temp_max)}℃</strong> ${Math.round(
+    forecast.main.temp_min
+  )}℃</div>
+    </div>`;
+}
+
 function searchOnLoad(city) {
   let apiKey = "5c3ee28f0359086dde5610bde74e2870";
   let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
   let fullUrl = `${apiUrl}${city}&units=metric&appid=${apiKey}`;
   axios.get(fullUrl).then(showTemperature);
+
+  fullUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(fullUrl).then(showForecast);
 }
 function displayCity(event) {
   event.preventDefault();
